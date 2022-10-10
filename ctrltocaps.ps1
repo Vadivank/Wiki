785f6ddf14f174748485e64d3b502da9b7f79495
Vadivank <68869494+Vadivank@users.noreply.github.com>
2022-10-10 13:36:32 +0300

    Delete ctrltocaps.ps1

--- ctrltocaps.ps1
+++
@@ -1,4 +0,0 @@
-$hexified = "00,00,00,00,00,00,00,00,03,00,00,00,3A,00,1D,00,1D,00,3A,00,00,00,00,00".Split(',') | % { "0x$_"};
-$kbLayout = 'HKLM:\System\CurrentControlSet\Control\Keyboard Layout';
-New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified);
-pause
